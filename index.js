196256c411d71683d2c3cb92674c3f432cd01f79
Tony <jdeleca@yahoo.es>
2022-06-10 08:01:46 -0400

    First one!

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const os = require('os');
+os.hostname = () => 'localhost';
+
+const Hapi = require('@hapi/hapi');
+
+const server = Hapi.server({
+    port: process.env.PORT || 3000
+});
+
+const init = async () => {
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return h.response('Hello home!');
+        }
+    });
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+    module.exports = server;
+};
+
+init();
